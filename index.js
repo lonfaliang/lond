3546d0d0f0686d18cc40ad0639d9e8612baf8859
473190078@qq.com <liangl00>
2019-04-29 11:50:20 +0800

    增加加密

--- index.js
+++ index.js
@@ -12,4 +12,20 @@ Lond.startRedis = function (options) {
     Lond.redis = require('./lib/redis')
 }
 Lond.lond = require('./lib/public')
+Lond.zip = require('./lib/lz') //消息压缩和解压
 module.exports = Lond
+
+
+// var process = require("process")
+//     version: process.version, //node版本
+//     platform: process.platform,//运行的操作系统的环境
+//     uptime: process.uptime(),//当前进程运行的时长（秒）
+//     pid: process.pid,//进程ID
+//     execPath: process.execPath,//当前node进程的执行路径
+//     cwd: process.cwd(), //当前工作目录
+//     rss: process.memoryUsage().rss, //当前使用内存情况
+//     heapTotal: process.memoryUsage().heapTotal,//googleV8内存使用情况
+//     heapUsed: process.memoryUsage().heapUsed,//googleV8内存使用情况
+//     external: process.memoryUsage().external, //V8管理的，绑定到Javascript的C++对象的内存使用情况
+//     cpu: process.arch ,//CPU架构
+//     startTime:startTime} //启动时间 毫秒
